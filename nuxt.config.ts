410f0a3635173fd54436d7f3bbf16c7fc2354ae4
Yonatan Kof <kof@yonatankof.com>
2024-07-04 19:10:41 +0300

    chore: Add @nuxt/image and @nuxthq/studio dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
-  extends: 'content-wind',
-  routeRules: {
-    '/': { prerender: true }
-  }
-})
+	extends: 'content-wind',
+
+	routeRules: {
+		'/': { prerender: true },
+	},
+
+	compatibilityDate: '2024-07-04',
+	modules: ['@nuxthq/studio', '@nuxt/image'],
+});
